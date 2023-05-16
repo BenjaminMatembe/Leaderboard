38f90ebde7024671ab42b74d61e313ebd2824213
MATA's Mac <matembebenjamin@gmail.com>
2023-05-16 23:31:57 +0300

    Add layout styles and fix errors

---
+++ dist/main.main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// // eslint-disable-next-line import/no-extraneous-dependencies\n// import _ from 'lodash';\n// import './styles.css';\n\n// function component() {\n//   const element = document.createElement('div');\n\n//   // Lodash, currently included via a script, is required for this line to work\n//   // eslint-disable-next-line no-undef\n//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');\n//   element.classList.add('hello');\n\n//   return element;\n// }\n\n// document.body.appendChild(component());\n\n\n//# sourceURL=webpack://leaderboard/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
